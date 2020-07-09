1d3e434481e48200d816d2309e4263c821d01b9e
Harshit Shukla <Harshitshukla151001@gmail.com>
2020-07-09 15:50:11 +0530

    useLocalstorage hook created

--- src/Components/Navbar/Mobile.js
+++ src/Components/Navbar/Mobile.js
@@ -1,7 +1,74 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Mobilenav = styled.div`
+  height: 9vh;
+  width: 100vw;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 2px 3px 10px 6px rgba(0, 0, 0, 0.5);
+  top: 0;
+  display: flex;
+  div {
+    width: 48%;
+    display: flex;
+    height: 100%;
+    background: white;
+    padding: 0 2% 0 2%;
+    align-items: center;
+    font-size: 2rem;
+    fontfamily: Roboto;
+  }
+`;
+const Drawer = styled.div``;
 
 function Mobile() {
-  return <></>;
+  return (
+    <Mobilenav>
+      <div>
+        <span
+          style={{
+            paddingRight: '10px',
+          }}
+        >
+          <i className="fa fa-bars"></i>
+        </span>
+        <span style={{ transform: 'translateY(-2px)' }}>
+          <p
+            style={{
+              fontFamily: 'Roboto',
+              fontSize: '2rem',
+            }}
+          >
+            <span
+              style={{
+                color: 'hsla(278, 100%, 50%, 1)',
+                fontFamily: 'Pacifico',
+              }}
+            >
+              Up
+            </span>
+            store
+          </p>
+        </span>
+      </div>
+      <div>
+        <span>
+          {' '}
+          <i className="fa fa-user-circle" aria-hidden="true"></i>
+        </span>
+        <span>
+          {' '}
+          <i className="fa fa-user-circle" aria-hidden="true"></i>
+        </span>
+        <span>
+          {' '}
+          <i className="fa fa-user-circle" aria-hidden="true"></i>
+        </span>
+      </div>
+    </Mobilenav>
+  );
 }
 
 export default Mobile;
