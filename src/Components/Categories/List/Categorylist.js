8bd73fdc4c4d88887318297c658d2dcaeae54d12
Harshit Shukla <Harshitshukla151001@gmail.com>
2020-07-17 20:00:21 +0530

    some changes done

--- src/Components/Categories/List/Categorylist.js
+++ src/Components/Categories/List/Categorylist.js
@@ -1,20 +1,30 @@
 //!library
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-
+//!custom hook
+//!import useWindowDimensions from '../../../customapis/useWindowDimensions';
 //!components
 import appContext from '../../../Statemanagement/Createcontext';
 import Categorycard from '../Cards/Categorycard';
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 const Categorylist = () => {
+  // const { width } = useWindowDimensions();
+  // const [x, setX] = useState(100);
+  // setX(() => {
+  //   return width > 991 ? 92.5 : 100;
+  // });
+
   const categorydata = useContext(appContext);
   return (
-    <Row style={{ width: '86%', margin: 'auto' }}>
+    <Row style={{ width: `92.5%`, margin: 'auto' }}>
       {categorydata.map(({ name, imagePath, _id }) => {
         return (
           <Col xs={6} md={4} key={_id}>
-            <Link style={{ textDecoration: 'none' }} to={`/products/${_id}/5eff8e76d75ecb3735b243b1`}>
+            <Link
+              style={{ textDecoration: 'none' }}
+              to={`/products/${_id}/5eff8e76d75ecb3735b243b1`}
+            >
               <Categorycard name={name} path={imagePath} id={_id} />
             </Link>
           </Col>
@@ -25,4 +35,3 @@ const Categorylist = () => {
 };
 
 export default Categorylist;
-
