9410480a9e949c258941bd3d27b475858146f250
Harshit Shukla <60488314+harshit151001@users.noreply.github.com>
2020-07-29 03:40:54 +0530

    otp done

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const app = express();
+
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function (req, res) {
+
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(3000);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
