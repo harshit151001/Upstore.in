ad65161acb808782608611e85a81d30e400bfa8f
Harshit Shukla <60488314+harshit151001@users.noreply.github.com>
2020-08-21 16:20:30 +0530

    carousel

--- src/Components/Carousel/Slider.js
+++ src/Components/Carousel/Slider.js
@@ -2,10 +2,7 @@ import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import ImgComp from './ImgComp';
 
-import i2 from '../Images/i2.jpg';
-import i3 from '../Images/i3.jpg';
-import i4 from '../Images/i4.jpg';
-import i5 from '../Images/i5.jpg';
+import i1 from '../Images/i1.png';
 
 const Slider = () => {
   const [index, setIndex] = useState(0);
@@ -15,16 +12,7 @@ const Slider = () => {
   return (
     <Carousel interval={1000} activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <ImgComp className="d-block w-100" src={i2} alt={`Second slide`} />
-      </Carousel.Item>
-      <Carousel.Item>
-        <ImgComp className="d-block w-100 " src={i3} alt={`Third slide`} />
-      </Carousel.Item>
-      <Carousel.Item>
-        <ImgComp className="d-block w-100" src={i4} alt={`Fourth slide`} />
-      </Carousel.Item>
-      <Carousel.Item>
-        <ImgComp className="d-block w-100" src={i5} alt={`Fifth slide`} />
+        <ImgComp className="d-block w-100" src={i1} alt={`Second slide`} />
       </Carousel.Item>
     </Carousel>
   );
