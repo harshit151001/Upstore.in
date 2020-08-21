3f6169a7b1858c9afb7de477c59083eef7699e89
Harshit Shukla <Harshitshukla151001@gmail.com>
2020-08-21 19:01:48 +0530

    commit

--- src/Components/Navbar/Mobile.js
+++ src/Components/Navbar/Mobile.js
@@ -33,7 +33,7 @@ const Mobile = () => {
     top: false,
     left: false,
     bottom: false,
-    right: false
+    right: false,
   });
   const [searchStyles, setSearchStyles] = React.useState({
     position: 'fixed',
@@ -43,7 +43,7 @@ const Mobile = () => {
     zIndex: '-1',
     display: 'none',
     background: 'white',
-    width: '0px'
+    width: '0px',
   });
   const handleSearch = () => {
     setSearchStyles({
@@ -55,18 +55,22 @@ const Mobile = () => {
       transform: 'translate(-5vw,-9vh)',
       zIndex: '12000',
       background: 'white',
-      width: '100%'
+      width: '100%',
     });
   };
   const closeSearch = () => {
     setSearchStyles({
       display: 'none',
-      width: '0px'
+      width: '0px',
     });
   };
   /****************************************************************************************************/
-  const toggleDrawer = (anchor, open) => event => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
       return;
     }
     setStyle({ ...style, [anchor]: open });
@@ -80,12 +84,15 @@ const Mobile = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       if (inputRef.current.value && enteredFilter === inputRef.current.value) {
-        const query = enteredFilter.length === 0 ? '' : `?search=${enteredFilter}`;
-        fetch(`${API}/api/search/products/5eff8e76d75ecb3735b243b1` + query).then(response => {
+        const query =
+          enteredFilter.length === 0 ? '' : `?search=${enteredFilter}`;
+        fetch(
+          `${API}/api/search/products/5eff8e76d75ecb3735b243b1` + query
+        ).then((response) => {
           response
             .json()
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
         });
       }
     }, 500);
@@ -94,15 +101,23 @@ const Mobile = () => {
     };
   }, [enteredFilter, inputRef]);
 
-  const list = anchor => (
-    <div style={{ width: '280px' }} role="presentation" onClick={toggleDrawer(anchor, false)} onKeyDown={toggleDrawer(anchor, false)}>
-      <List style={{ height: '70px' }}>
-        <Link style={{ textDecoration: 'none' }} to={!state.loggedIn ? '/loginsignup' : '/userdashboard'}>
+  const list = (anchor) => (
+    <div
+      style={{ width: '280px' }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List style={{ maxHeight: '50px' }}>
+        <Link
+          style={{ textDecoration: 'none' }}
+          to={!state.loggedIn ? '/loginsignup' : '/userdashboard'}
+        >
           <ListItem button>
             <ListItemIcon>
               <img
                 style={{
-                  height: '40px'
+                  height: '40px',
                 }}
                 src={userIcon}
                 alt="..."
@@ -114,19 +129,30 @@ const Mobile = () => {
                 fontSize: '20px',
                 fontStyle: 'italic',
                 color: '#0c0e21',
-                transform: 'translateY(9px)'
+                transform: 'translateY(9px)',
               }}
             >
-              {`Hi, ${state.loggedIn ? isAutheticated().user.name || isAutheticated().user.phoneNumber : 'there'}`}
-              <div>
-                {!state.loggedIn && (
-                  <Button fullWidth={true} style={{ backgroundColor: '#ec436f', color: 'white' }}>
-                    {' '}
-                    Login{' '}
-                  </Button>
-                )}
-              </div>
+              {`Hi, ${
+                state.loggedIn
+                  ? isAutheticated().user.name ||
+                    isAutheticated().user.phoneNumber
+                  : 'there'
+              }`}
             </p>
+            <span>
+              {!state.loggedIn && (
+                <Button
+                  fullWidth={true}
+                  style={{
+                    backgroundColor: '#ec436f',
+                    color: 'white',
+                    marginLeft: '10px',
+                  }}
+                >
+                  Login
+                </Button>
+              )}
+            </span>
           </ListItem>
         </Link>
       </List>
@@ -137,26 +163,38 @@ const Mobile = () => {
           style={{
             textDecoration: 'none',
             fontFamily: 'poppins',
-            color: 'inherit'
+            color: 'inherit',
           }}
         >
           <List>
-            <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/userdashboard">
+            <Link
+              style={{ textDecoration: 'none', color: 'inherit' }}
+              to="/userdashboard"
+            >
               <ListItem button>
                 <ListItemText> Dashboard</ListItemText>
               </ListItem>
             </Link>
-            <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/userdashboard/address">
+            <Link
+              style={{ textDecoration: 'none', color: 'inherit' }}
+              to="/userdashboard/address"
+            >
               <ListItem button>
                 <ListItemText> Addresses</ListItemText>
               </ListItem>
             </Link>
-            <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/userdashboard/orders">
+            <Link
+              style={{ textDecoration: 'none', color: 'inherit' }}
+              to="/userdashboard/orders"
+            >
               <ListItem button>
                 <ListItemText> Orders</ListItemText>
               </ListItem>
             </Link>
-            <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/userdashboard/details/edit">
+            <Link
+              style={{ textDecoration: 'none', color: 'inherit' }}
+              to="/userdashboard/details/edit"
+            >
               <ListItem button>
                 <ListItemText>Profile Details</ListItemText>
               </ListItem>
@@ -175,7 +213,7 @@ const Mobile = () => {
               style={{
                 textDecoration: 'none',
                 fontFamily: 'poppins',
-                color: 'inherit'
+                color: 'inherit',
               }}
             >
               <ListItem button key={_id}>
@@ -190,7 +228,7 @@ const Mobile = () => {
 
   return (
     <div>
-      {['left'].map(anchor => (
+      {['left'].map((anchor) => (
         <React.Fragment key={anchor}>
           <div
             style={{
@@ -203,12 +241,18 @@ const Mobile = () => {
               backdropFilter: 'blur(5px)',
               background: 'rgba(255, 255, 255, 0.9)',
               paddingRight: '4vw',
-              paddingLeft: '4vw'
+              paddingLeft: '4vw',
             }}
           >
-            <div className="row w-100 no-gutters align-items-center" style={{ height: '9vh' }}>
+            <div
+              className="row w-100 no-gutters align-items-center"
+              style={{ height: '9vh' }}
+            >
               <div className="col-6 no-gutters align-items-center d-flex">
-                <MenuRoundedIcon onClick={toggleDrawer(anchor, true)} style={{ color: 'rgba(20,20,20)', fontSize: '35px' }} />
+                <MenuRoundedIcon
+                  onClick={toggleDrawer(anchor, true)}
+                  style={{ color: 'rgba(20,20,20)', fontSize: '35px' }}
+                />
                 <span className="ml-1">
                   <Link to="/" style={{ textDecoration: 'none' }}>
                     <img
@@ -218,7 +262,7 @@ const Mobile = () => {
                         height: '6vh',
                         alignSelf: 'center',
                         transform: 'translateY(4px)',
-                        maxHeight: '40px'
+                        maxHeight: '40px',
                       }}
                     />
                   </Link>
@@ -227,34 +271,64 @@ const Mobile = () => {
               <div className="d-flex col-6 justify-content-end">
                 <Link>
                   <span className="mr-2">
-                    <SearchRoundedIcon onClick={handleSearch} style={{ color: 'rgba(20,20,20)', fontSize: '28px' }} />
+                    <SearchRoundedIcon
+                      onClick={handleSearch}
+                      style={{ color: 'rgba(20,20,20)', fontSize: '28px' }}
+                    />
                   </span>
                 </Link>
-                <Link to={!state.loggedIn ? '/loginsignup' : `/wishlist`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <Link
+                  to={!state.loggedIn ? '/loginsignup' : `/wishlist`}
+                  style={{ textDecoration: 'none', color: 'inherit' }}
+                >
                   <span className="mr-2">
-                    <FavoriteBorderRoundedIcon style={{ color: 'rgba(20,20,20)', fontSize: '28px' }} />
+                    <FavoriteBorderRoundedIcon
+                      style={{ color: 'rgba(20,20,20)', fontSize: '28px' }}
+                    />
                   </span>
                 </Link>
-                <Link to={!state.loggedIn ? '/loginsignup' : `/cart/${isAutheticated().user._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <Link
+                  to={
+                    !state.loggedIn
+                      ? '/loginsignup'
+                      : `/cart/${isAutheticated().user._id}`
+                  }
+                  style={{ textDecoration: 'none', color: 'inherit' }}
+                >
                   <span>
-                    <ShoppingCartOutlinedIcon style={{ color: 'rgba(20,20,20)', fontSize: '28px' }} />
+                    <ShoppingCartOutlinedIcon
+                      style={{ color: 'rgba(20,20,20)', fontSize: '28px' }}
+                    />
                   </span>
                 </Link>
               </div>
             </div>
-            <div className="col-12 w-100 ml-0 justify-content-center" style={{ ...searchStyles }}>
+            <div
+              className="col-12 w-100 ml-0 justify-content-center"
+              style={{ ...searchStyles }}
+            >
               <span className="mr-2">
-                <KeyboardBackspaceRoundedIcon onClick={closeSearch} style={{ fontSize: '35px' }} />
+                <KeyboardBackspaceRoundedIcon
+                  onClick={closeSearch}
+                  style={{ fontSize: '35px' }}
+                />
               </span>
               <form action="">
-                <Input ref={inputRef} value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)} style={{ width: '100%', fontSize: '26px' }} placeholder="search for products..." inputProps={{ 'aria-label': 'description' }} />
+                <Input
+                  ref={inputRef}
+                  value={enteredFilter}
+                  onChange={(event) => setEnteredFilter(event.target.value)}
+                  style={{ width: '100%', fontSize: '26px' }}
+                  placeholder="search for products..."
+                  inputProps={{ 'aria-label': 'description' }}
+                />
                 <Link
                   to={`/products/search?page=1&&search=` + enteredFilter}
                   style={{
                     textDecoration: 'none',
                     color: 'inherit',
                     display: 'flex',
-                    height: '5vh'
+                    height: '5vh',
                   }}
                 >
                   <button style={{ display: 'none' }} />
@@ -262,7 +336,12 @@ const Mobile = () => {
               </form>
             </div>
           </div>
-          <SwipeableDrawer anchor={anchor} open={style[anchor]} onClose={toggleDrawer(anchor, false)} onOpen={toggleDrawer(anchor, true)}>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={style[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
             {list(anchor)}
           </SwipeableDrawer>
         </React.Fragment>
